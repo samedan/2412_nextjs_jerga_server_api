fa8442487edae994f0116350dda1463d05c50263
John Doe <johndoe@example.com>
2024-12-16 16:18:57 +0100

    Express servers send back JSON

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+// load data file
+const path = require("path");
+const fs = require("fs");
+const pathToFile = path.resolve("./data.json");
+console.log(pathToFile);
+// END load data file
+
+const getResources = () => JSON.parse(fs.readFileSync(pathToFile));
+
 // endpoints
 app.get("/", (req, res) => {
   res.send("hello node js server");
 });
 app.get("/resources", (req, res) => {
-  res.send("resources  server");
+  const resources = getResources();
+  res.send(resources);
 });
+// END endpoints
 
 app.listen(PORT, () => {
   console.log("server listening on port: " + PORT);
