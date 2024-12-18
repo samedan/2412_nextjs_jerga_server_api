798110331a84c3caebb69304e61222fd2877c329
John Doe <johndoe@example.com>
2024-12-18 16:49:08 +0100

    Updated for deployment

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
-// const cors = require("cors");
 
-// var corsOptions = {
-//   origin: "http://localhost:3000",
-//   optionsSuccessStatus: 200,
-// };
-
-// load data file
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
 const pathToFile = path.resolve("./data.json");
-console.log(pathToFile);
-// END load data file
 
-// app.use(cors(corsOptions));
 const getResources = () => JSON.parse(fs.readFileSync(pathToFile));
 
-// get JSON
 app.use(express.json());
 
-// endpoints
 app.get("/", (req, res) => {
-  res.send("hello node js server");
+  res.send("Hello World");
 });
-app.get("/api/resources", (req, res) => {
+
+app.get("/api/resources/:id", (req, res) => {
   const resources = getResources();
-  res.send(resources);
+  const { id } = req.params;
+  const resource = resources.find((resource) => resource.id === id);
+  res.send(resource);
 });
+
+app.patch("/api/resources/:id", (req, res) => {
+  const resources = getResources();
+  const { id } = req.params;
+  const index = resources.findIndex((resource) => resource.id === id);
+  const activeResource = resources.find(
+    (resource) => resource.status === "active"
+  );
+
+  if (resources[index].status === "complete") {
+    return res
+      .status(422)
+      .send("Cannot update because resource has been completed!");
+  }
+
+  resources[index] = req.body;
+
+  // active resource related functionality
+  if (req.body.status === "active") {
+    if (activeResource) {
+      return res.status(422).send("There is active resource already!");
+    }
+
+    resources[index].status = "active";
+    resources[index].activationTime = new Date();
+  }
+  // active resource related functionality
+
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send("Cannot store data in the file!");
+    }
+
+    return res.send("Data has been updated!");
+  });
+});
+
 app.get("/api/activeresource", (req, res) => {
   const resources = getResources();
   const activeResource = resources.find(
@@ -36,70 +64,30 @@ app.get("/api/activeresource", (req, res) => {
   );
   res.send(activeResource);
 });
-app.get("/api/resources/:id", (req, res) => {
+
+app.get("/api/resources", (req, res) => {
   const resources = getResources();
-  const resourceId = req.params.id;
-  const resource = resources.find((item) => item.id === resourceId);
-  res.send(resource);
-});
-app.patch("/api/resources/:id", (req, res) => {
-  const resources = getResources();
-  const { id } = req.params;
-  const index = resources.findIndex((item) => item.id === id);
-
-  // only one ActiveResource
-  const activeResource = resources.find(
-    (resource) => resource.status === "active"
-  );
-  // check if Resource is marked as Complete
-  if (resources[index].status === "complete") {
-    return res
-      .status(422)
-      .send("Cannot update because resource has been completed.");
-  }
-
-  resources[index] = req.body;
-  if (activeResource) {
-    console.log("activeResoucre found");
-    console.log(activeResource);
-  }
-  if (req.body.status === "active") {
-    if (activeResource) {
-      return res.status(422).send("Already active resource");
-    }
-    resources[index].status = "active";
-    resources[index].activationTime = new Date();
-  }
-  // END only one ActiveResource
-
-  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
-    if (error) {
-      return res.status(422).send("Cannot store data in file");
-    } else {
-      return res.send("Data updated");
-    }
-  });
+  res.send(resources);
 });
 
 app.post("/api/resources", (req, res) => {
   const resources = getResources();
   const resource = req.body;
+
   resource.createdAt = new Date();
   resource.status = "inactive";
   resource.id = Date.now().toString();
-  // resources.push(resource);
   resources.unshift(resource);
 
   fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
     if (error) {
-      return res.status(422).send("Cannot store data in file");
-    } else {
-      return res.send("Data saved");
+      return res.status(422).send("Cannot store data in the file!");
     }
+
+    return res.send("Data has been saved!");
   });
 });
-// END endpoints
 
 app.listen(PORT, () => {
-  console.log("server listening on port: " + PORT);
+  console.log("Server is listening on port:" + PORT);
 });
